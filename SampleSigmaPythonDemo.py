738932efed145b6ae2abae3df2c2884143ef70a9
surajpadhy <suraj_padhy@hotmail.com>
2021-05-11 19:06:20 +0530

    Initial Commit

--- SampleSigmaPythonDemo.py
+++ SampleSigmaPythonDemo.py
@@ -1,7 +1,27 @@
+import boto3
+ddb = boto3.client("dynamodb")
 import json
+
 def handler(event, context):    
     return {"message": "Successfully executed"}
 
-def add_user():
+def add_user(event, context):
     data = {"a":"abcd"}
-    datastr = json.d
+    datastr = json.dumps(data)
+    print(datastr)
+
+    try:
+        recepie = ddb.get_item(
+            TableName="Recepie",
+            Key={
+                'recepie_id': {
+                    'S': recepie_id
+                }
+            }
+        )
+    except BaseException as e:
+        print(e)
+        raise(e)
+    return {"message":"Retrieved Recepie", "recepie":recepie}
+    
+
